20a2c3698bbbe03c4992d2122b3acc8b758fa55f
kb developer <kbis_kpi@ukr.net>
2016-11-03 15:49:38 +0200

    Init project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name directoryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the directoryApp
+ */
+angular.module('directoryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name directoryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the directoryApp
+ */
+angular.module('directoryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
